4b2f02b9149ba625ca954ee1a3bc904f3c2a0e4d
fred <fredfrede414@gmail.com>
2019-11-03 19:32:37 +0400

    added one signal

---
+++ Assets/Scenes&Scripts/Game/OneSignalMine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneSignalMine : MonoBehaviour
+{
+    void Start()
+    {
+        // Enable line below to enable logging if you are having issues setting up OneSignal. (logLevel, visualLogLevel)
+        //OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.INFO, OneSignal.LOG_LEVEL.INFO);
+
+        OneSignal.StartInit("eaa86f0c-b1b7-4ed2-beed-7dd5cad89abc")
+          .HandleNotificationOpened(HandleNotificationOpened)
+          .EndInit();
+
+        OneSignal.inFocusDisplayType = OneSignal.OSInFocusDisplayOption.Notification;
+    }
+
+    // Gets called when the player opens the notification.
+    private static void HandleNotificationOpened(OSNotificationOpenedResult result)
+    {
+
+    }
+}
